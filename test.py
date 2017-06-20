dbceed2be2b46152d3f88cc828324ad5f6a272c7
Axial CI <axial-ci@users.noreply.github.com>
2017-06-20 17:41:28 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 # This program adds up integers in the command line
 import sys
 try:
-     total = sum(int(arg) for arg in sys.argv[1:])
+          total = sum(int(arg) for arg in sys.argv[1:])
     print 'sum =', total
-except ValueError:
-    print 'Please supply integer arguments'
+ except ValueError:
+     print 'Please supply integer arguments'
